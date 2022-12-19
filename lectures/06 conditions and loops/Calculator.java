76871bdf6731d44ce0a8b858457cc1349e026759
bahjaisack <115221500+bahjaisack@users.noreply.github.com>
2022-12-19 13:08:31 +0300

    Update Calculator.java

--- lectures/06 conditions and loops/Calculator.java
+++ lectures/06 conditions and loops/Calculator.java
@@ -1,19 +1,14 @@
-package com.kunal;
-
 import java.util.Scanner;
 
 public class Calculator {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        // Take input from user till user does not press X or x
         int ans = 0;
         while (true) {
-            // take the operator as input
             System.out.print("Enter the operator: ");
             char op = in.next().trim().charAt(0);
 
             if (op == '+' || op == '-' || op == '*' || op == '/' || op == '%') {
-                // input two numbers
                 System.out.print("Enter two numbers: ");
                 int num1 = in.nextInt();
                 int num2 = in.nextInt();
