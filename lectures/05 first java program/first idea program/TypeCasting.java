a46adb3d78d2af7b65769bf331c2f83293db7871
bahjaisack <115221500+bahjaisack@users.noreply.github.com>
2022-12-19 13:06:39 +0300

    Update TypeCasting.java

--- lectures/05 first java program/first idea program/TypeCasting.java
+++ lectures/05 first java program/first idea program/TypeCasting.java
@@ -1,45 +1,41 @@
-package com.kunal;
-
 import java.util.Scanner;
 
 public class TypeCasting {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-//        float num = input.nextFloat();
-//        int num = input.nextInt();
-//        System.out.println(num);
+        float num = input.nextFloat();
+        int num = input.nextInt();
+        System.out.println(num);
 
-        // type casting
-        int num = (int)(67.56f);
-//        System.out.println(num);
+  
+      int num = (int)(67.56f);
+        System.out.println(num);
 
-        // automatic type promotion in expressions
-//        int a = 257;
-//        byte b = (byte)(a); // 257 % 256 = 1
+        int a = 257;
+        byte b = (byte)(a); // 257 % 256 = 1
 
-//        byte a = 40;
-//        byte b = 50;
-//        byte c = 100;
-//        int d = a * b / c;
-//
-//        System.out.println(d);
+        byte a = 40;
+        byte b = 50;
+        byte c = 100;
+        int d = a * b / c;
 
-//        byte b = 50;
-//        b = b * 2;
+        System.out.println(d);
 
-//        int number = 'A';
-//        System.out.println("你好");
+        byte b = 50;
+        b = b * 2;
 
-//        System.out.println(3 * 6);
+        int number = 'A';
+        System.out.println("你好");
 
-        byte b = 42;
-        char c = 'a';
-        short s = 1024;
-        int i = 50000;
+        System.out.println(3 * 6);
+
+      byte b = 42;
+      char c = 'a';
+      short s = 1024;
+      int i = 50000;
         float f = 5.67f;
         double d = 0.1234;
         double result = (f * b) + (i / c) - (d * s);
-        // float + int - double = double
         System.out.println((f * b) + " " + (i / c) + " " + (d * s));
         System.out.println(result);
     }
