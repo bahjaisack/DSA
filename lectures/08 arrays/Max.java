f6a3ffc09ae7b2cca0abcf5ebc5b5164e62aca32
bahjaisack <115221500+bahjaisack@users.noreply.github.com>
2022-12-19 08:17:05 +0300

    Update Max.java

--- lectures/08 arrays/Max.java
+++ lectures/08 arrays/Max.java
@@ -1,12 +1,8 @@
-package com.kunal;
-
 public class Max {
     public static void main(String[] args) {
         int[] arr = {1, 3, 2, 9, 18};
         System.out.println(maxRange(arr, 1, 3));
     }
-
-    // work on edge cases here, like array being null
     static int maxRange(int[] arr, int start, int end) {
 
         if (start > end) {
