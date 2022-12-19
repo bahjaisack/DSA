37be3132053daa0bdfc251087e246b33cb244c79
bahjaisack <115221500+bahjaisack@users.noreply.github.com>
2022-12-19 08:12:27 +0300

    Update Input.java

--- lectures/08 arrays/Input.java
+++ lectures/08 arrays/Input.java
@@ -1,4 +1,3 @@
-package com.kunal;
 
 import java.util.Arrays;
 import java.util.Scanner;
@@ -6,8 +5,6 @@ import java.util.Scanner;
 public class Input {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-
-        // array of primitives
         int[] arr = new int[5];
         arr[0] = 23;
         arr[1] = 45;
@@ -16,34 +13,30 @@ public class Input {
         arr[4] = 3;
         // [23, 45, 233, 543, 3]
         System.out.println(arr[3]);
-
-        // input using for loops
         for (int i = 0; i < arr.length; i++) {
             arr[i] = in.nextInt();
         }
-//
-//        System.out.println(Arrays.toString(arr));
 
-//        for (int i = 0; i < arr.length; i++) {
-//            System.out.print(arr[i] + " ");
-//        }
+        System.out.println(Arrays.toString(arr));
 
-//        for(int num : arr) { // for every element in array, print the element
-//            System.out.print(num + " "); //  here num represents element of the array
-//        }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
 
-//        System.out.println(arr[5]); // index out of bound error
+        for(int num : arr) { // for every element in array, print the element
+            System.out.print(num + " "); //  here num represents element of the array
+      }
 
-        // array of objects
+        System.out.println(arr[5]); // index out of bound error
+
+         array of objects
         String[] str = new String[4];
         for (int i = 0; i < str.length; i++) {
             str[i] = in.next();
         }
 
         System.out.println(Arrays.toString(str));
-
-        // modify
-        str[1] = "kunal";
+        str[1] = "bahja";
 
         System.out.println(Arrays.toString(str));
     }
